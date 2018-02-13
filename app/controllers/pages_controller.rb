380ecabbbf5321bd661a87d9a5d8cdb8cc424042
Milenko Castillo <milenko.castillo@gmail.com>
2018-02-13 15:25:36 -0300

    Primer commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def services
+  end
+end
---
+++ config/initializers/assets.rb
@@ -0,0 +1,24 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'css', 'images')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'sass')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'sass', 'libs')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'images')
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w( *.jpg *.png )
+Rails.application.config.assets.precompile += %w( *.svg *.eot *.woff *.tff *.woff2 )
